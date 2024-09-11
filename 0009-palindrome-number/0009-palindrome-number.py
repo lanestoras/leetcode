dc9c54fda86be7d1f196704a851c1929a21a2005
lanestoras <88054152+lanestoras@users.noreply.github.com>
2024-09-11 13:55:24 +0300

    Time: 195 ms (5.77%), Space: 16.6 MB (40.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,15 +1,34 @@
 class Solution:
+    
     def isPalindrome(self, x: int) -> bool:
+        palin=0
+        num=x
+        rev=0
+        while num>0:
+            dig=num%10
+            print("dig=",dig)
+            rev=rev*10+dig
+            print("rev=",rev)
+            num=int(num/10)
+            print("num=",num)
+        return (rev==x)
 
-        array=self.toArray(x)
+
+
+    def isPalindromeFast(self, x: int) -> bool:
+        s=str(x)
+        if x>=0:
+           return s == s[::-1]
+        else:
+            return False
+
+    def isPalindrome_first(self, x: int) -> bool:
+        array=toArray(x)
         revArray=array[::-1]
         return array == revArray
-        
 
-    def toArray(self, x:int) -> list:
-        array=[]
-        for i in str(x):
-            array.append(i)
-        return array
-
- 
+        def toArray(self, x:int) -> list:
+            array=[]
+            for i in str(x):
+                array.append(i)
+            return array
